191da11856bdf4fd88718333d384535f8666beb0
varma <varma.cloud.devops@gmail.com>
2026-03-09 19:44:12 +0530

    Added: Code Base

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './index.css';
 
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL ;
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
 
 function App() {
   const [todos, setTodos] = useState([]);
